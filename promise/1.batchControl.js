50f21e3f2a9c2a03f691a3bf3ff85e07213b069b
wine-fall <62830944+wine-fall@users.noreply.github.com>
2024-04-02 16:38:10 +0800

    Update 1.batchControl.js

--- promise/1.batchControl.js
+++ promise/1.batchControl.js
@@ -2,9 +2,13 @@
  * mock ajax request
  */
 
-const timeout = time => new Promise((resolve) => {
-    setTimeout(resolve, time);
-}); 
+const mockAjax = (time, order) => {
+    return new Promise((res) => {
+        setTimeout(() => {
+            res(order);
+        }, time);
+    });
+};
 
 /**
  * 1 - 并发控制
@@ -12,7 +16,7 @@ const timeout = time => new Promise((resolve) => {
  * 初始化传入一个 num 为最大并发量
  * 达到同时并发数不能超过该最大并发量，若小于则从队列中取出下一个异步来执行
  */
- class Schedule{
+class Schedule{
 
     constructor(maxNum){
         this.list = [];
@@ -39,15 +43,53 @@ const timeout = time => new Promise((resolve) => {
 
 const schedule = new Schedule(2);
 
-const addTask = (time, order) => {
-    schedule.add(() => timeout(time).then(() => {
-        console.log(order);
-    }));
+/**
+ * @param {{() => Promise}[]} promises 
+ * @param {number} maxNum
+ * @returns {Promise}
+ */
+const control = (promises, maxNum) => {
+    const queue = [...promises];
+    const n = promises.length;
+    return new Promise((resolve, reject) => {
+        let c = 0;
+        const ret = [];
+        const run = () => {
+            if (ret.length === n) {
+                resolve(ret);
+                return;
+            }
+            if (queue.length && c < maxNum) {
+                c++;
+                const fn = queue.shift();
+                fn().then((v) => {
+                    ret.push(v);
+                    c--;
+                    run();
+                }).catch((err) => {
+                    reject(err);
+                });
+            }
+        };
+        for (let i = 0; i < promises.length; i++) {
+            run();
+        };
+    });
 };
 
-addTask(1000, 1);
-addTask(500, 2);
-addTask(600, 3);
-addTask(400, 4);
+const tasks = [
+    [1000, 1],
+    [500, 2],
+    [600, 3],
+    [400, 4]
+];
+
+// for (let i = 0; i < tasks.length; i++) {
+//     schedule.add(() => mockAjax(...tasks[i]));
+// };
+
+control(tasks.map((input) => () => mockAjax(...input)), 2).then((res) => {
+    console.log(res);
+});
 
 // 2 1 3 4
